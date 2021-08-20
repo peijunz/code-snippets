68bd193f87f13363c1d15a5f4e76b65db4bb98fd
Peijun Zhu <zpj.ustc@gmail.com>
2021-08-20 15:44:09 -0700

    add linear init

--- bit.py
+++ bit.py
@@ -1,24 +1,29 @@
 from operator import add
-class BIT:
-    def __init__(self, n, combine=add, cons=int):
-        self.indexes = []
+class BIT:  
+    def __init__(self, vals, combine=add, cons=int):
+        if not isinstance(vals, list):
+            vals = [cons() for i in range(vals)]
+        n = len(vals)
+        self.indexes = [vals]
         self.combine = combine
         self.cons = cons
-        
-        while n > 0:
-            self.indexes.append([self.cons() for i in range(n)])
-            if n==1:
-                break
+        while n > 1:
             n = (n+1)//2
+            i = len(self.indexes)
+            self.indexes.append([self.recompute(i, k) for k in range(n)])
+
+    def recompute(self, i, k):
+        prev = self.indexes[i-1]
+        if 2*k+1<len(prev):
+            return self.combine(prev[2*k], prev[2*k+1])
+        else:
+            return prev[2*k]
 
     def __setitem__(self, k, v):
         self.indexes[0][k] = v
-        for prev, cur in zip(self.indexes[:-1], self.indexes[1:]):
+        for i, cur in enumerate(self.indexes[1:], 1):
             k //= 2
-            if 2*k+1<len(prev):
-                cur[k] = self.combine(prev[2*k], prev[2*k+1])
-            else:
-                cur[k] = prev[2*k]
+            cur[k] =  self.recompute(i, k)
 
     def __getitem__(self, i):
         return self.indexes[0][i]
@@ -38,6 +43,15 @@ class BIT:
         return [self.prefix(i) for i in range(len(self.indexes[0])+1)]
 
 if __name__=='__main__':
+    """
+    Expected
+    ========
+    Array: [49, 97, 53, 5, 33, 65, 62, 51, 100]
+    Sum: [0, 49, 146, 199, 204, 237, 302, 364, 415, 515]
+    Max: [0, 49, 97, 97, 97, 97, 97, 97, 97, 100]
+    Min: [inf, 49, 49, 49, 5, 5, 5, 5, 5, 5]
+    """
+
     from random import randint, seed
     seed(0)
 
